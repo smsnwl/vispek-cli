66ca96789357a68517bfa1d6675b8e690e4b2906
smsnwl <39265300+smsnwl@users.noreply.github.com>
2018-08-29 14:25:56 +0800

    Add files via upload

--- cli.py
+++ cli.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
             call('fswebcam -d /dev/video%d cam%d.jpg -q'%(i*2,i*2), shell=True)
             call('cp cam%d.jpg %d-cam%d.jpg'%((i*2),ts,(i*2)), shell=True)
             call('touch %d-nir.dat'%ts, shell=True)
-        call(['ssh', 'pi@192.168.0.112', '/home/pi/conveyor/enqueue.py', '0','%d'%c])
-        call(['curl', 'http://192.168.0.110:8080/repeat/1'])
+        call(['ssh', 'pi@192.168.0.112', '/home/pi/conveyor/enqueue.py', '1','%d'%c])
+        call(['curl', 'http://192.168.0.110:8080/hello'])
